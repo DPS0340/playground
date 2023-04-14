292fa0ab8e317242ddf448259b72e477e83fd5ef
Jiho Lee <optional.int@kakao.com>
2023-04-14 23:56:41 +0900

    chore: remove todo, inline transpose ituple

--- src/comprehensive-rust/src/exercise_7_2.rs
+++ src/comprehensive-rust/src/exercise_7_2.rs
@@ -1,16 +1,11 @@
-// TODO: remove this when you're done with your implementation.
-#![allow(unused_variables, dead_code)]
-
-fn conv(x: usize, y: usize) -> (usize, usize) {
-    (y, x)
-}
+#![allow(dead_code)]
 
 pub fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
     let mut res: [[i32; 3]; 3] = [[0; 3]; 3];
 
     matrix.iter().enumerate().for_each(|(i, row)| {
         row.iter().enumerate().for_each(|(j, _)| {
-            let (i2, j2) = conv(i, j);
+            let (j2, i2) = (i, j);
             res[i][j] = matrix[i2][j2];
         });
     });
