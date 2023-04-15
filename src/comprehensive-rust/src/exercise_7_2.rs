6661d509f2bacbded41d0e81d7bdb71be4715dc0
Jiho Lee <optional.int@kakao.com>
2023-04-15 19:30:43 +0900

    feat: matrix transpose using generic len parameter

--- src/comprehensive-rust/src/exercise_7_2.rs
+++ src/comprehensive-rust/src/exercise_7_2.rs
@@ -1,10 +1,12 @@
 #![allow(dead_code)]
 
-pub fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
-    let mut res: [[i32; 3]; 3] = [[0; 3]; 3];
+use core::fmt;
 
-    matrix.iter().enumerate().for_each(|(i, row)| {
-        row.iter().enumerate().for_each(|(j, _)| {
+pub fn transpose<const R: usize, const C: usize>(matrix: [[i32; R]; C]) -> [[i32; C]; R] {
+    let mut res: [[i32; C]; R] = [[0; C]; R];
+
+    (0..C).for_each(|j| {
+        (0..R).for_each(|i| {
             let (j2, i2) = (i, j);
             res[i][j] = matrix[i2][j2];
         });
@@ -13,7 +15,7 @@ pub fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
     res
 }
 
-pub fn pretty_print(matrix: &[[i32; 3]; 3]) {
+pub fn pretty_print<T: fmt::Display, const R: usize, const C: usize>(matrix: &[[T; R]; C]) {
     print!("{{");
     matrix.iter().enumerate().for_each(|(i, row)| {
         print!("{{");
