8cd1e81dba5346059a28bf44470eb6cd65c2539c
Jiho Lee <optional.int@kakao.com>
2023-04-15 23:35:59 +0900

    refactor: test code on implementation file

--- src/comprehensive-rust/src/exercise_7_1.rs
+++ src/comprehensive-rust/src/exercise_7_1.rs
@@ -1,3 +1,11 @@
-pub fn multiply(x: i16, y: i16) -> i16 {
+fn multiply(x: i16, y: i16) -> i16 {
     x * y
+}
+
+#[test]
+fn test() {
+    let x: i8 = 15;
+    let y: i16 = 1000;
+
+    assert_eq!(i16::from(x) * y, multiply(x.into(), y));
 }
--- src/comprehensive-rust/src/exercise_7_2.rs
+++ src/comprehensive-rust/src/exercise_7_2.rs
@@ -1,35 +1,57 @@
-#![allow(dead_code)]
-
 use core::fmt;
 
-pub fn transpose<T: Default + Copy, const R: usize, const C: usize>(
+fn transpose<T: Default + Copy, const R: usize, const C: usize>(
     matrix: [[T; R]; C],
 ) -> [[T; C]; R] {
     let mut res = [[T::default(); C]; R];
 
-    (0..C).for_each(|j| {
-        (0..R).for_each(|i| {
+    for j in 0..C {
+        for i in 0..R {
             res[i][j] = matrix[j][i];
-        });
-    });
+        }
+    }
 
     res
 }
 
-pub fn pretty_print<T: fmt::Display, const R: usize, const C: usize>(matrix: &[[T; R]; C]) {
-    print!("{{");
+fn pretty_print<T: fmt::Display, const R: usize, const C: usize>(matrix: &[[T; R]; C]) {
+    print!("[");
     matrix.iter().enumerate().for_each(|(i, row)| {
-        print!("{{");
+        print!("[");
         row.iter().enumerate().for_each(|(i, e)| {
             print!("{}", e);
             if i != row.len() - 1 {
                 print!(", ");
             }
         });
-        print!("}}");
+        print!("]");
         if i != matrix.len() - 1 {
             println!();
         }
     });
-    println!("}}");
+    println!("]");
+}
+
+#[test]
+fn test() {
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+        [401, 402, 403],
+    ];
+    let transposed_answer = [
+        [101, 201, 301, 401], // same
+        [102, 202, 302, 402],
+        [103, 203, 303, 403],
+    ];
+
+    println!("matrix:");
+    pretty_print(&matrix);
+
+    let transposed = transpose(matrix);
+    println!("transposed:");
+    pretty_print(&transposed);
+
+    assert_eq!(transposed, transposed_answer);
 }
