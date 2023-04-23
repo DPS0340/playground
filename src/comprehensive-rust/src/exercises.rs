8dadf1d08f26c0039436d8b1216833907b511cb4
Jiho Lee <optional.int@kakao.com>
2023-04-23 19:48:27 +0900

    feat: solve exercise 26.1

--- src/comprehensive-rust/src/exercises.rs
+++ src/comprehensive-rust/src/exercises.rs
@@ -3,5 +3,6 @@ pub mod exercise_17_1;
 pub mod exercise_17_2;
 pub mod exercise_21_1;
 pub mod exercise_21_2;
+pub mod exercise_26_1;
 pub mod exercise_7_1;
 pub mod exercise_7_2;
---
+++ src/comprehensive-rust/src/exercises/exercise_26_1.rs
@@ -0,0 +1,117 @@
+// TODO: remove this when you're done with your implementation.
+#![allow(unused_imports, unused_variables, dead_code)]
+
+pub trait Widget {
+    /// Natural width of `self`.
+    fn width(&self) -> usize;
+
+    /// Draw the widget into a buffer.
+    fn draw_into(&self, buffer: &mut dyn std::fmt::Write);
+
+    /// Draw the widget on standard output.
+    fn draw(&self) {
+        let mut buffer = String::new();
+        self.draw_into(&mut buffer);
+        println!("{buffer}");
+    }
+}
+
+pub struct Label {
+    label: String,
+}
+
+impl Label {
+    fn new(label: &str) -> Label {
+        Label {
+            label: label.to_owned(),
+        }
+    }
+}
+
+pub struct Button {
+    label: Label,
+    callback: Box<dyn FnMut()>,
+}
+
+impl Button {
+    fn new(label: &str, callback: Box<dyn FnMut()>) -> Button {
+        Button {
+            label: Label::new(label),
+            callback,
+        }
+    }
+}
+
+pub struct Window {
+    title: String,
+    widgets: Vec<Box<dyn Widget>>,
+}
+
+impl Window {
+    fn new(title: &str) -> Window {
+        Window {
+            title: title.to_owned(),
+            widgets: Vec::new(),
+        }
+    }
+
+    fn add_widget(&mut self, widget: Box<dyn Widget>) {
+        self.widgets.push(widget);
+    }
+
+    fn inner_width(&self) -> usize {
+        std::cmp::max(
+            self.title.chars().count(),
+            self.widgets.iter().map(|w| w.width()).max().unwrap_or(0),
+        )
+    }
+}
+
+impl Widget for Label {
+    fn width(&self) -> usize {
+        (self.label.len() + 2).max(15)
+    }
+
+    fn draw_into(&self, buffer: &mut dyn std::fmt::Write) {
+        let width = self.width();
+        let s = "=".to_string().repeat(width);
+        buffer.write_fmt(format_args!("{}\n", s)).unwrap();
+        buffer
+            .write_fmt(format_args!("|{:^width$}|\n", self.label))
+            .unwrap();
+        buffer.write_fmt(format_args!("{}\n", s)).unwrap();
+    }
+}
+
+impl Widget for Button {
+    fn width(&self) -> usize {
+        self.label.width()
+    }
+
+    fn draw_into(&self, buffer: &mut dyn std::fmt::Write) {
+        self.label.draw_into(buffer);
+    }
+}
+
+impl Widget for Window {
+    fn width(&self) -> usize {
+        self.inner_width() + 2
+    }
+
+    fn draw_into(&self, buffer: &mut dyn std::fmt::Write) {
+        self.widgets.iter().for_each(|e| {
+            e.draw_into(buffer);
+        })
+    }
+}
+
+#[test]
+fn test() {
+    let mut window = Window::new("Rust GUI Demo 1.23");
+    window.add_widget(Box::new(Label::new("This is a small text GUI demo.")));
+    window.add_widget(Box::new(Button::new(
+        "Click me!",
+        Box::new(|| println!("You clicked the button!")),
+    )));
+    window.draw();
+}
