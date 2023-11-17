269a7acc73de9f7b632b26f7734bd66830f4a08a
Jiho Lee <optional.int@kakao.com>
2023-11-17 23:07:58 +0900

    feat: add home-manager-practice

---
+++ src/nix/home-manager-practice/01-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Enable debug output
+PS4="\n\033[1;33m>>\033[0m "; set -x
+
+LOCATION=$(realpath "$0")
+DIR=$(dirname "$LOCATION")
+
+# Cache sudo credential
+sudo -v
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+
+nix-shell '<home-manager>' -A install
---
+++ src/nix/home-manager-practice/99-uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Enable debug output
+PS4="\n\033[1;33m>>\033[0m "; set -x
+
+LOCATION=$(realpath "$0")
+DIR=$(dirname "$LOCATION")
+
+# Cache sudo credential
+sudo -v
+
+home-manager uninstall
+nix-env --uninstall home-manager
